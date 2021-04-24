4bb3a8a27f84814d4de288d99c53bb577ca54e7c
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-04-24 19:03:37 +0200

    Add new endpoints. Modify app.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const loggedUser = (req, res, next) => {
+  if (req.user == undefined) {
+    res.redirect('/user/no-permission');
+  } else {
+    next();
+  };
+};
+
+router.get('/logged', loggedUser, (req, res) => {
+  res.render('logged', {
+    name: req.user.name.givenName,
+    lastName: req.user.name.familyName,
+    avatar: req.user.photos[0].value
+  });
+});
+
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  req.user ? res.render('logged') : res.redirect('no-permission');
+  console.log('req.user logged', req.user);
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', loggedUser, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', loggedUser, (req, res) => {
+  res.render('settings');
 });
 
 module.exports = router;
